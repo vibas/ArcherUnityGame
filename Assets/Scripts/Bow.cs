0c3e7ca2d703190cb2953f9c50961b34dc776267
vibas <vibas.behera095@gmail.com>
2023-09-30 22:39:11 +0530

    Arrow trajectory path added

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -1,42 +1,73 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
     Camera mainCamera;
     Vector2 bowPos;
+    Vector2 bowDirection;
 
     [SerializeField] BowString bowString;
 
     [SerializeField] GameObject arrowPrefab;
     GameObject activeArrow = null;
     [SerializeField] Transform shotPoint;
-    private float shootForce = 20;
+    [SerializeField] float shootForce;
+    [SerializeField] float shootForceMultiplier;
+
+    [SerializeField] GameObject pointPrefab;
+    private GameObject[] points;
+    [SerializeField] int numberOfPoints;
+    [SerializeField] float spaceBetweenPoints;
+
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
         bowPos = transform.position;
+
+        points = new GameObject[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i] = Instantiate(pointPrefab,transform.position, Quaternion.identity);
+            points[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePos - bowPos;
-        transform.right = direction;
+        float mouseDistanceFromBow = Vector2.Distance(mousePos,bowPos);
+        bowDirection = mousePos - bowPos;
+        transform.right = bowDirection;
 
         if(Input.GetMouseButtonDown(0))
         {
             activeArrow = PrepareArrow();
         }
 
+        if(Input.GetMouseButton(0))
+        {
+            shootForce = mouseDistanceFromBow * shootForceMultiplier;
+
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                points[i].SetActive(true);
+                points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+            }
+        }
+
         if(Input.GetMouseButtonUp(0))
         {
             if(activeArrow)
                 Shoot();
+
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                points[i].SetActive(false);
+            }
         }
 
     }
@@ -58,5 +89,13 @@ public class Bow : MonoBehaviour
         rb.simulated = true;
         rb.velocity = transform.right * shootForce;
         bowString.Release();
+    }
+
+    Vector2 PointPosition(float t)
+    {
+        Vector2 pointPos = (Vector2)transform.position + 
+                           (bowDirection.normalized * shootForce * t)
+                            + 0.5f * Physics2D.gravity * (t * t); 
+        return pointPos;
     }
 }
